ef8b9d739f9a4742e0de154f6d272af0159e90ef
lakshya009 <lakshyayadav3009.ly@gmail.com>
2022-10-01 13:27:13 +0530

    native

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Header/Header";
+import Section from "./Section/Section";
+import './App.css'
+
+const App = () => {
+  return (<>
+    <Header />
+    <Section/>
+  </>
+  );
+
+};
+export default App;
